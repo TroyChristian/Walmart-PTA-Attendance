3ac094b8c8ce99f068c01c10115342b18afc492b
Nkisi <nkisi@pop-os.localdomain>
2024-12-06 10:05:55 -0500

    Add script tag to tracker overview

--- WPA/tracker/urls.py
+++ WPA/tracker/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
 path('trackers', views.attendance_tracker_overview, name="trackers"),
 path('create', views.create_attendance_tracker, name="create"),
-<<<<<<< HEAD
+
 path('tracker/<int:tracker_pk>', views.attendance_tracker_view, name="tracker"),
 path('tracker/<int:tracker_pk>/team/<uuid:team_pk>', views.team_attendance_view, name="team_attendance"), #TODO take_attendance_view
 path('associates/<int:associate_pk>', views.associate_detail_view, name="associate_detail"),
